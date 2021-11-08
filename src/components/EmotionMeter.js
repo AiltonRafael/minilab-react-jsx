5aa404f3e83199d73b90cde38516f7300c2ab878
Ailton Rafael <ailton.junior@al.infnet.edu.br>
2021-11-08 15:31:39 -0300

    updated

--- src/components/EmotionMeter.js
+++ src/components/EmotionMeter.js
@@ -4,11 +4,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSmile, faFrownOpen} from '@fortawesome/free-solid-svg-icons'
 
 export default function EmotionMeter(){
-    let emotion = 'SAD'
+    let emotion = ['HAPPY', "SAD"]
+    let indexRandom = Math.floor(Math.random()*emotion.length)
     
     return (
         <Box d='flex' justifyContent='center' mt='10px'> 
-                {emotion === 'HAPPY' ? <FontAwesomeIcon icon={faSmile} /> : <FontAwesomeIcon icon={faFrownOpen}/>}
+                {emotion[indexRandom] === 'HAPPY' ? <FontAwesomeIcon icon={faSmile} size='6x'/> : <FontAwesomeIcon icon={faFrownOpen} size='6x' />}
         </Box>
     )
 }
